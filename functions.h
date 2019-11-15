87f2a8d1fbd57d64d95db3874f5566b1e3fff330
tectonicboy <49075948+tectonicboy@users.noreply.github.com>
2019-11-15 02:21:38 +0000

    Update functions.h

--- functions.h
+++ functions.h
@@ -4,120 +4,230 @@
 #include <string>
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
-constexpr unsigned short int max_size = 512;
 
 using namespace std;
+
+typedef vector<bool>::size_type bvec_size;
+typedef vector<bool> bvec;
 
 class bin_num {
 
 public:
-	unsigned short int size;
-	bool N[max_size];
-	bool MSB, LSB;
+	bvec_size size;
+	bvec N;
+	bool MSB;
+	bool LSB;
+	string name = "";
 	//Constructor from a string containing 0s and 1s.
 	bin_num(string S) : size(S.size()) {
-		short int index = 0;
-		for (int i = 0; i < S.size(); ++i) {
-			if (S[i] == '0') { N[index] = false; }
-			else if (S[i] == '1') { N[index] = true; }
+		for (bvec_size i = 0; i < S.size(); ++i) {
+			if (S[i] == '0') { N.push_back(false); }
+			else if (S[i] == '1') { N.push_back(true); }
 			else { cout << "WARNING: Construction string contains characters other than '0' and '1'.\n"; }
-			++index;
 		}
 		MSB = N[S.size() - 1];
 		LSB = N[0];
 	};
 
 	//Constructor from a vector of bools.
-	bin_num(vector<bool>S) : size(S.size()), LSB(*(S.begin())), MSB(*(S.end() - 1)) {
-		short int index = 0;
-		for (int i = 0; i < S.size(); ++i) {
-			if (S[i] == 0) { N[index] = false; }
-			else { N[index] = true; }
-			++index;
+	bin_num(bvec S) : size(S.size()), LSB(*(S.begin())), MSB(*(S.end() - 1)) {
+		for (bvec_size i = 0; i < S.size(); ++i) {
+			if (!S[i]) { N.push_back(false); }
+			else { N.push_back(true); }
 		}
+		MSB = &(N[S.size() - 1]);
+		LSB = N[0];
 	}
-	//Default constructor.
-	bin_num() : size(max_size), MSB(false), LSB(false) {};
+	//Constructor from a size. Initializes all bits to 0.
+	bin_num(bvec_size k) : size(k), MSB(false), LSB(false) {
+		for (bvec_size i = 0; i < k; ++i) {
+			N.push_back(0);
+		}
+	};
 
-
-	void Print_Num() {
-		for (int i = 0; i < this->size; ++i) {
-			cout << this->N[i] << " ";
+	
+	void Print_Num() const {
+		for (bvec_size i = 0; i < this->size; ++i) {
+			cout << this->N[i];
 		}
 		cout << "\n";
 	}
-};
 
-bin_num smaller(const bin_num &X, const bin_num &Y) {
-	if (X.size > Y.size) { return Y; }
-	else { return X; }
-}
+	void Update() {
+		while (!*(N.end() - 1)) {
+			N.erase(N.end() - 1);
+			--size;
+		}
+		LSB = *(N.begin());
+		MSB = *(N.end() - 1);
+		size = N.size();
+	}
 
-bin_num bigger(const bin_num &X, const bin_num &Y) {
-	if (X.size > Y.size) { return X; }
-	else { return Y; }
-}
+	void Negate() {
+		bvec_size i = 0;
+		while (i < this->size) {
+			if (this->N[i]) { this->N[i] = false; }
+			else { this->N[i] = true; }
+			++i;
+		}
+		cout << "\t\t\tInverted the number: ";
+		this->Print_Num();
+		i = 0;
+		bool found = false;
+		while (i < this->size && !found) {
+			if (this->N[i]) {
+				this->N[i] = false;
+			}
+			else {
+				this->N[i] = true;
+				found = true;
+			}
+			++i;
+		}
+		if (!found) {
+			this->N.push_back(true);
+			++this->size;
+		}
+		cout << "\t\t\tAdded one to it: ";
+		this->Print_Num();
 
-bin_num Add_Two(const bin_num &X, const bin_num &Y) {
-	bin_num* p1 = &(bigger(X, Y));
-	bin_num* p2 = &(smaller(X, Y));
-	bool carry = false;
-	bin_num R;
-	short int counter = -1;
-	for (int i = 0; i <= (*p2).size - 1; ++i) {
-		++counter;
-		if (carry) {
-			if ((X.N[i] && !Y.N[i]) || (!X.N[i] && Y.N[i])) {
-				R.N[i] = false;
-				carry = true;
-			}
-			else if (!X.N[i] && !Y.N[i]) {
-				R.N[i] = true;
-				carry = false;
-			}
-			else if (X.N[i] && Y.N[i]) {
-				R.N[i] = true;
-				carry = true;
-			}
+	}
+
+
+	bin_num* smaller(bin_num &X) {
+		bin_num* ptr;
+		if (X.size > this->size) { 
+			ptr = this;
+			return ptr;
 		}
 		else {
-			if ((X.N[i] && !Y.N[i]) || (!X.N[i] && Y.N[i])) {
-				R.N[i] = true;
-				carry = false;
-			}
-			else if (!X.N[i] && !Y.N[i]) {
-				R.N[i] = false;
-				carry = false;
-			}
-			else if (X.N[i] && Y.N[i]) {
-				R.N[i] = false;
-				carry = true;
-			}
+			ptr = &X;
+			return ptr;;
 		}
 	}
 
-	++counter;
-	while (carry) {
-		if ((*p1).N[counter]) {
-			R.N[counter] = false;
+	bin_num* bigger(bin_num &X) {
+		bin_num* ptr;
+		if (X.size > this->size) {
+			ptr = &X;
+			return ptr;
 		}
 		else { 
-			R.N[counter] = true;
-			break;
+			ptr = this;
+			return ptr;
 		}
-		++counter;
-	}
-	++counter;
-
-	while (counter <= (*p1).size - 1) {
-		R.N[counter] = (*p1).N[counter];
-		++counter;
 	}
 
-	R.size = (*p1).size;
-	return R;
-}
+	bin_num operator+ (bin_num & X) {
+		this->name = "Katya", X.name = "Boiko";
+		bin_num* p1;
+		bin_num* p2;
+		if (this->size != X.size) {
+			p1 = (X.bigger(*this));
+			p2 = (X.smaller(*this));
+		}
+		else {
+			p1 = &X;
+			p2 = &(*this);
+		}
+		cout << "p1 is pointing to " << p1->name << ", p2 is pointing to " << p2->name << "\n";
+		cout << "Numbers we're adding:\n";
+		for (bvec_size i = 0; i < p1->size; ++i) {
+			cout << p1->N[i];
+		}
+		cout << "\n";
+		for (bvec_size i = 0; i < p2->size; ++i) {
+			cout << p2->N[i];
+		}
+		cout << "\n";
+		bool carry = false;
+		bin_num R(p1->size);
+		short int counter = 0;
+		for (bvec_size i = 0; i <= p2->size - 1; ++i) {
+			++counter;			
+			if (carry) {
+				if ((X.N[i] && !this->N[i]) || (!X.N[i] && this->N[i])) {
+					R.N[i] = false;
+					carry = true;
+				}
+				else if (!X.N[i] && !this->N[i]) {
+					R.N[i] = true;
+					carry = false;
+				}
+				else if (X.N[i] && this->N[i]) {
+					R.N[i] = true;
+					carry = true;
+				}
+			}
+			else {
+				if ((X.N[i] && !this->N[i]) || (!X.N[i] && this->N[i])) {
+					R.N[i] = true;
+					carry = false;
+				}
+				else if (!X.N[i] && !this->N[i]) {
+					R.N[i] = false;
+					carry = false;
+				}
+				else if (X.N[i] && this->N[i]) {
+					R.N[i] = false;
+					carry = true;
+				}
+			}
+		}
+		while ((carry) && (counter != p1->size)) {
+			if (p1->N[counter]) {
+				R.N[counter] = false;
+			}
+			else {
+				R.N[counter] = true;
+				carry = false;
+				break;
+			}
+			++counter;
+		}
+		if (carry) { R.N.push_back(true); }
+		++counter;
+
+		while (counter <= p1->size - 1) {
+			R.N[counter] = p1->N[counter];
+			++counter;
+		}
+
+		R.Update();
+		return R;
+	}
+
+	bin_num operator- (bin_num X) {
+		cout << "Input number is: ";
+		X.Print_Num();
+		X.Negate();
+		cout << "Negated number is now: ";
+		X.Print_Num();
+		cout << "\n";
+		bin_num R = *this + X;
+		cout << "And Result is: ";
+		R.Print_Num();
+		return R;
+	}
+
+	bin_num operator* (bin_num & X) {
+		bin_num R(this->size + X.size), Aux = *this;
+		if (X.N[0]) { R = R + Aux; }
+		for (bvec_size i = 1; i < X.size; ++i) {
+			Aux.N.insert(Aux.N.begin(), 1, 0);
+			++Aux.size;
+			if (X.N[i]) {
+				R = R + Aux;
+			}
+		}
+		R.Update();
+		return R;
+	}
+
+
+};
 
 
 #endif
