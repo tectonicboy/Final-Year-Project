1d4e212cd3250a69f23f6e6197e06984066bfbb0
tectonicboy <49075948+tectonicboy@users.noreply.github.com>
2019-11-28 17:53:38 +0000

    Update simult_solver.h
    Implemented Euclid's algorithm and so the algorithm to find Lowest Common Multiple of two numbers.

--- simult_solver.h
+++ simult_solver.h
@@ -6,12 +6,68 @@
 
 using namespace std;
 
+//The template function to find the Lowest Common Multiple of two numbers (whatever their encoding system is)
+template <typename T>
+T LCM(T a, T b) {
+	T r0 = max(a, b), r1 = min(a, b), r2(0), gcd(0), lcm(0), aux(0), zero(0);
+	bool b1 = true;
+	vector<T> V;
+	int x = 0;
+	while (x == 0) {
+		aux = r0 - r1;
+		V.push_back(aux);
+		while (aux >= zero) {
+
+			aux -= r1;
+			V.push_back(aux);
+		}
+		if ((*(V.end() - 2)) == zero) {
+			gcd = *(V.end() - 3);
+			break;
+		}
+		if (b1) {
+			if (abs(*(V.end() - 1)) < *(V.end() - 2)) {
+				b1 = false;
+				r2 = abs(*(V.end() - 1));
+			}
+			else {
+				b1 = true;
+				r2 = *(V.end() - 2);
+			}
+		}
+		else {
+			b1 = true;
+			if (*(V.end() - 2) < *(V.end() - 3)) {
+				r2 = *(V.end() - 2);
+			}
+			else {
+				r2 = *(V.end() - 3);
+			}
+		}
+		r0 = r1, r1 = r2;
+		V = {};
+	}
+	T c = (a * b);
+	lcm = abs(c);
+	T counter(0);
+	while (lcm > zero) {
+		++counter;
+		lcm -= gcd;
+	}
+	return counter;
+}
+
+//Just so I can test it with ints
+void Nullify(int & X){
+	X = 0;
+}
+
+
 /*The template for solving a system of simultaneous equations is made such that the coefficients of the unknowns are of type T.
 Furthermore, the template function assumes that the following functionality for type T exists:
 0. There is a function called Nullify which intakes a variable of type T and makes it equal to zero.
-1. There is a function called LCM that intakes two arguments of type T as input and finds their lowest common multiple, outputting a 3rd variable of type T.
-2. operator-=, operator>=, operator++, operator--, operator+ and operator- are defined for type T.
-3. You can instantiate a variable of type T as zero by writing T var_name(0), where 0 is an int.
+1. operator-=, operator>=, operator++, operator--, operator+ and operator- are defined for type T.
+2. You can instantiate a variable of type T equaling zero by writing T var_name(0), where 0 is an int.
 */
 template <typename T>
 void Solve (vector<vector<T>> V, int r, int c){
@@ -21,7 +77,7 @@ void Solve (vector<vector<T>> V, int r, int c){
 		while (r_curr <= r - 1) {
 			lcm = LCM(V[r_pivot][c_pivot], V[r_curr][c_pivot]);
 			aux = lcm;
-			while(aux >= V[r_pivot][c_pivot]){
+			while(aux >= V[r_pivot][c_pivot]){ 
 				++counter;
 				aux -= V[r_pivot][c_pivot];
 			}
@@ -44,6 +100,7 @@ void Solve (vector<vector<T>> V, int r, int c){
 			Nullify(counter);
 			for (int F = 0; F < c; ++F) {
 				while (V[r_pivot][F] >= aux) {
+	
 					++counter;
 					V[r_pivot][F] -= aux;
 				}
@@ -57,18 +114,18 @@ void Solve (vector<vector<T>> V, int r, int c){
 		}
 		++c_pivot;
 		++r_pivot;
-		r_curr = r_pivot + 1;
+		r_curr = r_pivot; 
+		++r_curr;
 	}
 
 	r_pivot = r - 1, c_pivot = c - 2, r_curr = r_pivot - 1;
 	while (c_pivot > 0) {
-
 		while (r_curr >= 0) {
 			lcm = LCM(V[r_pivot][c_pivot], V[r_curr][c_pivot]);
 			aux = lcm;
 			while (aux >= V[r_pivot][c_pivot]) {
 				++counter;
-				aux -= V[r_pivot][c_pivot]
+				aux -= V[r_pivot][c_pivot];
 			}
 			aux = counter;
 			Nullify(counter);
@@ -83,7 +140,7 @@ void Solve (vector<vector<T>> V, int r, int c){
 			aux = lcm;
 			while (aux >= V[r_curr][c_pivot]) { 
 				++counter;
-				aux -= V[r_curr][c_pivot] 
+				aux -= V[r_curr][c_pivot];
 			}
 			aux = counter;
 			Nullify(counter);
@@ -117,20 +174,5 @@ void Solve (vector<vector<T>> V, int r, int c){
 
 }
 
-//The template function to find the Lowest Common Multiple of two numbers (whatever their encoding system is)
 
-template <typename T>
-T LCM(T a, T b) {
-	T r0 = max(a, b), r1 = min(a, b), r2, e0, e1, e2, gcd, lcm;
-
-	//Implement Euclid's Algorithm for finding Greatest Common Divisor here.
-
-	lcm = abs(a, b);
-	T counter(0);
-	while (lcm > 0) {
-		++counter;
-		lcm -= gcd;
-	}
-	return counter;
-}
 #endif
