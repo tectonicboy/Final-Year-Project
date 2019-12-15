e51cf426b2379f021ad3195ede1f0f67e4725c03
tectonicboy <49075948+tectonicboy@users.noreply.github.com>
2019-12-15 13:03:20 +0200

    Update simult_solver.h
    LCM seems to be fine but Solve needs more fixing.

--- simult_solver.h
+++ simult_solver.h
@@ -8,9 +8,22 @@ using namespace std;
 
 //The template function to find the Lowest Common Multiple of two numbers (whatever their encoding system is), implementing Euclid's Algorithm.
 template <typename T>
-T LCM(T a, T b) {
-	T r0 = max(a, b), r1 = min(a, b), r2(0), gcd(0), lcm(0), aux(0), zero(0), r(0), e(0);
-	cout << "Worked out MAX = "; r0.Print_Num(); cout << ", MIN = "; r1.Print_Num(); cout << "\n";
+T LCM(T &a, T &b) {
+	bool b1 = false, b2 = false;
+	T zero(0);
+	if (a < zero) {
+		b1 = true;
+		a.Negate();
+	}
+	if (b < zero) {
+		b2 = true;
+		b.Negate();
+	}
+	if ((a == zero) || (b == zero)) {
+		cout << "ERROR: LCM can't take zero as input.\n";
+		return zero;
+	}
+	T r0 = max(a, b), r1 = min(a, b), r2(0), gcd(0), lcm(0), aux(0), r(0), e(0);
 	bool wasE = false;
 	vector<T> V;
 	int x = 0;
@@ -26,20 +39,17 @@ T LCM(T a, T b) {
 			}
 			if ((*(V.end() - 2)) == zero) {
 				gcd = r1;
-				cout << "\t\t**** GCD = "; gcd.Print_Num(); cout << " *****\n";
 				goto l1;
 			}
 			r2 = *(V.end() - 1);
 		}
 		else {
-			cout << "\tIt was not E.\n";
 			while (aux >= zero) {
 				aux -= r1;
 				V.push_back(aux);
 			}
 			if ((*(V.end() - 2)) == zero) {
 				gcd = r1;
-				cout << "\t\t***** GCD = "; gcd.Print_Num(); cout << " *****\n";
 				goto l1;
 			}
 			e = abs(aux);
@@ -54,130 +64,128 @@ T LCM(T a, T b) {
 			}
 			
 		}
-		cout << "\t\tNEW R = "; r2.Print_Num(); cout << "\n";
 		r0 = r1, r1 = r2;
 		V = {};
 	}
 	l1:
 	T c = (a * b);
 	lcm = abs(c);
-	T counter(0);
-	int count = 0;
-	cout << "Lastly, dividing abs(a*b) = "; lcm.Print_Num(); cout << " BY gcd = "; gcd.Print_Num(); cout << "\n";
-	while (lcm > zero) {
-		++count;
-		if (count % 1000 == 0) { cout << "    COUNT = " << count << "\n"; }
-		++counter;
-		lcm -= gcd;
-	}
-	cout << "END COUNT = " << count << "\n";
-	return counter;
+	lcm /= gcd;
+	if (b1) { a.Negate(); }
+	if (b2) { b.Negate(); }
+	cout << "LCM = "; lcm.Print_Num(); cout << "\n";
+	return lcm;
 }
 
 
-void Nullify(int & X){
-	X = 0;
-}
 /*The template for solving a system of simultaneous equations is made such that the coefficients of the unknowns are of type T.
 Furthermore, the template function assumes that the following functionality for type T already exists:
 0. There is a function with signature "void Nullify(T)" which makes the input number equal to zero.
 1. operator-=, operator>=, operator++, operator--, operator+ and operator- are defined for type T.
-2. You can instantiate a variable of type T equaling zero by writing "T var_name(0)", where 0 is an int.
+2. You can initialize a variable of type T equaling zero by writing "T var_name(0)", where 0 is an int.
 */
 template <typename T>
-void Solve (vector<vector<T>> V, int r, int c){
+void Solve (vector<vector<T>> &V, int r, int c){
 	int r_curr = 1, c_pivot = 0, r_pivot = 0;
-	T aux, lcm, counter(0);
+	T lcm(0), pivot(0), current(0), pivaux(0), curraux(0), zero(0), XX(0), YY(0);
+	vector<T> PivotRow, CurrentRow;
 	//Loop to triangulate the matrix.
-	while (c_pivot < c - 1) {
+	while (c_pivot < c - 2) {
+		PivotRow = V[r_pivot];
+		pivot = V[r_pivot][c_pivot];
 		//Loop defining what happens at each row.
 		while (r_curr <= r - 1) {
-			lcm = LCM(V[r_pivot][c_pivot], V[r_curr][c_pivot]);
-			aux = lcm;
-			while(aux >= V[r_pivot][c_pivot]){ 
-				++counter;
-				aux -= V[r_pivot][c_pivot];
-			}
-			aux = counter;
-			Nullify(counter);
-			for (int F = 0; F < c; ++F) {
-				while (V[r_curr][F] >= aux) {
-					++counter;
-					V[r_curr][F] -= aux;
+			cout << "Entered. row_current = " << r_curr << "\n";
+			CurrentRow = V[r_curr];
+			current = V[r_curr][c_pivot];
+			if (current != zero) {
+				if (current == pivot) {
+					goto label1;
 				}
-				V[r_curr][F] = counter;
-				Nullify(counter);
-			}
-			aux = lcm;
-			while (aux >= V[r_curr][c_pivot]) {
-				++counter;
-				aux -= V[r_curr][c_pivot];
-			}
-			aux = counter;
-			Nullify(counter);
-			for (int F = 0; F < c; ++F) {
-				while (V[r_pivot][F] >= aux) {
-					++counter;
-					V[r_pivot][F] -= aux;
+				if (pivot == zero) {
+					for (auto i = 0; (i < PivotRow.size()); ++i) {
+						CurrentRow[i] *= PivotRow[i];
+					}
 				}
-				V[r_pivot][F] = counter;
-				Nullify(counter);
-			}
-			for (int F = 0; F < c; ++F) {
-				V[r_curr][F] -= V[r_pivot][F];
+				else {
+					lcm = LCM(pivot, current);
+					XX = abs(pivot);
+					YY = abs(current);
+					pivaux = lcm/XX ;
+					curraux = lcm/YY;
+					for (auto i = 0; i < PivotRow.size(); ++i) {
+						PivotRow[i] *= pivaux;
+						CurrentRow[i] *= curraux;
+					}
+					label1:
+					//Both signs are the same.
+					if ( ((pivot > zero) && (current > zero)) || ((pivot < zero) && (current < zero)) ) {
+						for (auto i = 0; i < PivotRow.size(); ++i) {
+							CurrentRow[i] -= PivotRow[i];
+						}
+					}
+					//The signs are different.
+					else if (((pivot > zero) && (current < zero)) || ((pivot < zero) && (current > zero))) {
+						for (auto i = 0; i < PivotRow.size(); ++i) {
+							CurrentRow[i] += PivotRow[i];
+						}
+					}
+					V[r_curr] = CurrentRow;
+				}
 			}
 			++r_curr;
 		}
 		++c_pivot;
 		++r_pivot;
-		r_curr = r_pivot; 
-		++r_curr;
+		r_curr = r_pivot + 1; 
+	}
+	cout << "The matrix after 1st reduction:\n";
+	for (auto i = 0; i < V.size(); ++i) {
+		for (auto j = 0; j < (V[i]).size(); ++j) {
+			(V[i][j]).Print_Num(); cout << " ";
+		}
+		cout << "\n";
 	}
 	r_pivot = r - 1, c_pivot = c - 2, r_curr = r_pivot - 1;
-	//Loop to triangulate the matrix again, producing an integer multiple of the solution matrix.
+	//Loop to triangulate the matrix again.
 	while (c_pivot > 0) {
+		PivotRow = V[r_pivot];
+		pivot = V[r_pivot][c_pivot];
 		//Loop defining what happens at each row.
 		while (r_curr >= 0) {
-			lcm = LCM(V[r_pivot][c_pivot], V[r_curr][c_pivot]);
-			aux = lcm;
-			while (aux >= V[r_pivot][c_pivot]) {
-	
-				++counter;
-				aux -= V[r_pivot][c_pivot];
-			}
-			aux = counter;
-			Nullify(counter);
-			for (int F = 0; F < c; ++F) {
-	
-				while (V[r_curr][F] >= aux) {
-	
-					++counter;
-					V[r_curr][F] -= aux;
+			cout << "Entered. row_current = " << r_curr << "\n";
+			CurrentRow = V[r_curr];
+			current = V[r_curr][c_pivot];
+			if (current != zero) {
+				if (pivot == zero) {
+					for (auto i = 0; i < PivotRow.size(); ++i) {
+						CurrentRow[i] *= PivotRow[i];
+					}
 				}
-				V[r_curr][F] = counter;
-				Nullify(counter);
-			}
-			aux = lcm;
-			while (aux >= V[r_curr][c_pivot]) { 
-
-				++counter;
-				aux -= V[r_curr][c_pivot];
-			}
-			aux = counter;
-			Nullify(counter);
-			for (int F = 0; F < c; ++F) {
-	
-				while (V[r_pivot][F] >= aux) {
-	
-					++counter;
-					V[r_pivot][F] -= aux;
+				else {
+					lcm = LCM(pivot, current);
+					XX = abs(pivot);
+					YY = abs(current);
+					pivaux = lcm / XX;
+					curraux = lcm / YY;
+					for (auto i = 0; i < PivotRow.size(); ++i) {
+						PivotRow[i] *= pivaux;
+						CurrentRow[i] *= curraux;
+					}
+					//Both signs are the same.
+					if (((pivot > zero) && (current > zero)) || ((pivot < zero) && (current < zero))) {
+						for (auto i = 0; i < PivotRow.size(); ++i) {
+							CurrentRow[i] -= PivotRow[i];
+						}
+					}
+					//The signs are different.
+					else if (((pivot > zero) && (current < zero)) || ((pivot < zero) && (current > zero))) {
+						for (auto i = 0; i < PivotRow.size(); ++i) {
+							CurrentRow[i] += PivotRow[i];
+						}
+					}
+					V[r_curr] = CurrentRow;
 				}
-				V[r_pivot][F] = counter;
-				Nullify(counter);
-			}
-			for (int F = 0; F < c; ++F) {
-
-				V[r_curr][F] -= V[r_pivot][F];
 			}
 			--r_curr;
 		}
@@ -185,17 +193,17 @@ void Solve (vector<vector<T>> V, int r, int c){
 		--r_pivot;
 		r_curr = r_pivot - 1;
 	}
-	//Loop to produce the final solution matrix.
-	for (int F = 0; F < r; ++F) {
-		while (V[F][c - 1] >= V[F][F]) {
-			++counter;
-			V[F][c - 1] -= V[F][F];
+	cout << "The matrix after 2nd reduction:\n";
+	for (auto i = 0; i < V.size(); ++i) {
+		for (auto j = 0; j < (V[i]).size(); ++j) {
+			(V[i][j]).Print_Num(); cout << " ";
 		}
-		V[F][c - 1] = counter;
-		Nullify(counter);
-		V[F][F] = 1;
+		cout << "\n";
+	}
+	//Loop to produce the final solution matrix by dividing at each row.
+	for (int F = 0; F < r; ++F) {
+		V[F][c - 1] /= V[F][F];
+		V[F][F] /= V[F][F];
 	}
 }
-
-
 #endif
